296bf3814c7611f5ab22c2116059df0cdf4d8618
2tankov2 <tankov.toha@hotmail.com>
2019-05-29 11:59:41 +0300

    убирает скрипты вотдельный файл

---
+++ source/js/main-menu_button.js
@@ -0,0 +1,44 @@
+
+          var links = document.querySelectorAll(".catalog-item__cart");
+            var popup = document.querySelector(".modal-order");
+
+            for (var i = 0; i < links.length; i++) {
+               links[i].addEventListener("click", function (evt) {
+                 evt.preventDefault();
+                 popup.classList.add("modal-show");
+               });
+            }
+
+            var link = document.querySelector(".product-week__order");
+
+            var popup = document.querySelector(".modal-order");
+
+
+            link.addEventListener("click", function (evt) {
+              evt.preventDefault();
+              popup.classList.add("modal-show");
+
+          });
+
+
+            var navMain = document.querySelector('.main-nav');
+            var navToggle = document.querySelector('.main-nav__toggle');
+
+            navMain.classList.remove('main-nav--nojs');
+
+            navToggle.addEventListener('click', function() {
+              if (navMain.classList.contains('main-nav--closed')) {
+                navMain.classList.remove('main-nav--closed');
+                navMain.classList.add('main-nav--opened');
+
+                navToggle.classList.remove('main-nav__toggle--closed');
+                navToggle.classList.add('main-nav__toggle--opened');
+              } else {
+                navMain.classList.add('main-nav--closed');
+                navMain.classList.remove('main-nav--opened');
+
+
+                navToggle.classList.add('main-nav__toggle--closed');
+                navToggle.classList.remove('main-nav__toggle--opened');
+              }
+            });
